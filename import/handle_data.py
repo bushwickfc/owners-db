31301df484422a9d51fe72a86d95b6d73f5e7849
Darren Klein <darren.a.klein@gmail.com>
2018-05-25 00:49:49 -0400

    Refactor, some comments

--- import/handle_data.py
+++ import/handle_data.py
@@ -1,27 +1,54 @@
 # This module will get data from Google Sheets and format it as a list of dicts
-import util
+import time
 
-# Pull some important values out of each line of the master db
-def process_master_db_data(rd):
-    return dict({}, pos_id=rd[1], email=util.normalize_email(rd[5]))
+# Lowercase email addresses and remove whitespace.
+def normalize_email(email):
+    return email.lower().replace(" ", "")
+
+def create_display_name(first_name, last_name):
+    return first_name + ' ' + last_name
+
+# Convert a Google Sheet timestamp string ('5/1/2018 21:07:52') to basic date string format ('2018-5-1')
+def timestamp_to_date(ts):
+    dt_tup = time.strptime(ts, "%m/%d/%Y %H:%M:%S")
+    return str(dt_tup[0]) + "-" + str(dt_tup[1]) + "-" + str(dt_tup[2])
+
+# Clean up the building/street/unit number fields so we have a cohesive address.
+def format_address(building, street, unit):
+    building_street = building + ' ' + street
+    return building_street if unit == '' else building_street + ', ' + unit
+
+# Pull some important values out of each line of the master db - we want to associate POS IDs
+# from the old database with new owher accounts, so make a dict with the email as the key and POS ID as the value
+def process_master_db_data(master_db_raw_data):
+    master_db_dict = {}
+    for rd in master_db_raw_data:
+        master_db_dict[normalize_email(rd[5])] = rd[1]
+
+    return master_db_dict
+
+# From the master db data, attempt to find the matching record from the new owner signup
+# by matching on (normalized) email address. If there's a match, return the pos_id for that record.
+def get_pos_id_by_email(master_db_data, email):
+    return master_db_data[email] if email in master_db_data and master_db_data[email] != '' else None
 
 # From each row and the data_dict, create a dictionary for each owner
 def process_raw_data(owner, processed_master_db_data, data_dict):
-    email = util.normalize_email(owner[3])
+    email = normalize_email(owner[3])
 
-    return dict(data_dict, join_date=util.timestamp_to_date(owner[0]),
-                           pos_id=util.get_pos_id_by_email(processed_master_db_data, email),
+    return dict(data_dict, join_date=timestamp_to_date(owner[0]),
+                           pos_id=get_pos_id_by_email(processed_master_db_data, email),
                            first_name=owner[1],
                            last_name=owner[2],
                            email=email,
-                           display_name=util.create_display_name(owner[1], owner[2]),
+                           display_name=create_display_name(owner[1], owner[2]),
                            phone=owner[4],
-                           address=util.format_address(owner[7], owner[8], owner[9]),
+                           address=format_address(owner[7], owner[8], owner[9]),
                            city=owner[10],
                            state=owner[11],
                            zipcode=owner[12])
 
 def execute(new_owner_raw_data, master_db_raw_data, data_dict):
     print("Processing raw data...")
-    processed_master_db_data = [process_master_db_data(rd) for rd in master_db_raw_data]
+    processed_master_db_data = process_master_db_data(master_db_raw_data)
     return [process_raw_data(rd, processed_master_db_data, data_dict) for rd in new_owner_raw_data]
--- import/import_from_google_sheet.py
+++ import/import_from_google_sheet.py
@@ -3,13 +3,12 @@ import pygsheets
 # Get the raw data as a matrix
 def execute():
     print('Fetching data from Google Sheets...')
-    # Authorize access to Google Drive/Google Sheet API
-    # https://pygsheets.readthedocs.io/en/latest/authorizing.html
+    # Authorize access to Google Drive/Google Sheet API - https://pygsheets.readthedocs.io/en/latest/authorizing.html
     gc = pygsheets.authorize(outh_file='client_secret.json', outh_nonlocal=True)
-    # Get the sheets by name
+    # Get the sheets by name - the new owner sheet for most of the info, older member db for additional info (POS ID, banked hours...)
     new_owner_sheet = gc.open('Copy of New Owner Onboarding')
     member_database = gc.open('Copy of BFC Member Database (ACTIVE)')
-    # Get the specific worksheets
+    # Get the specific worksheets from each sheet
     new_owner_worksheet = new_owner_sheet.worksheet_by_title('All New Owners')
     master_db_worksheet = member_database.worksheet_by_title('MASTER DB')
     # Pull the values from the sheets as a matrix
