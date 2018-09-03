4eb7d22ae439f3c5f8de419a8b40246510f5be0d
Kevin Qiu <kevin@arena.io>
2018-09-03 16:34:31 -0400

    add pkey

--- sql/V7__Add_precision_hour_log.sql
+++ sql/V7__Add_precision_hour_log.sql
@@ -1,1 +1,5 @@
-alter table hour_log alter column hour_date set data type timestamp(0)
+alter table hour_log drop constraint hour_log_pkey;
+
+alter table hour_log add column id serial primary key;
+
+create index email_hour_log_hour_reason on hour_log (email, hour_reason);
