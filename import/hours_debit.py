bf4b40cbe4a1ac910787466419b7d1e2c6fcec2e
Kevin Qiu <kevin@arena.io>
2018-11-08 16:55:28 -0500

    account for end date

--- import/hours_debit.py
+++ import/hours_debit.py
@@ -8,7 +8,7 @@ def existing(conn, date):
         cursor.execute(query, (date,))
         return cursor.fetchone()
 
-# TODO: we only want to debit people who signed up before the 15 of last mongth
+
 def debit(conn, date):
     if existing(conn, date):
         print('Hour debit already run for date, skipping')
@@ -17,10 +17,16 @@ def debit(conn, date):
     query = """insert into hour_log (email, amount, hour_reason, hour_date)
                (select cot.email, -ot.work_requirement, 'monthly_requirement',
                 %s
-               from current_owner_type cot
+               from (select distinct
+                            email,
+                            first_value(owner_type) over
+                              (partition by email order by start_date desc)
+                            as owner_type
+                    from owner_owner_type
+                    where end_date > %s or end_date is null) cot
                join owner_type ot on cot.owner_type = ot.owner_type
                join owner_equity_type oet on oet.email = cot.email
                where oet.start_date < %s)"""
     with conn.cursor() as cursor:
-        cursor.execute(query, (date, cutoff))
+        cursor.execute(query, (date, date, cutoff))
         print('Hours debited')
